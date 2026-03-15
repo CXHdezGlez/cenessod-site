87460d4dd956aa89505b43d3a5282bfe093bfb2f
CXHdezGlez <106573376+CXHdezGlez@users.noreply.github.com>
2026-03-15 04:51:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll(".fade-up").forEach(el => {
   observer.observe(el);
 });
+
+
+<script>
+window.addEventListener("scroll", function(){
+const header = document.querySelector("header");
+
+if(window.scrollY > 50){
+header.classList.add("scrolled");
+}else{
+header.classList.remove("scrolled");
+}
+});
+</script>
